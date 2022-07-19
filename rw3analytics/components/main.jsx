b18a1a5df73ce362bb8245b4eea1a435e4175c6d
MSBivens <mikebiv95@gmail.com>
2022-07-18 19:27:47 -0500

    status update for stage 1

---
+++ rw3analytics/components/main.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { NFTCard } from "../components/nftCard";
+
+const Main = () => {
+  const [collection, setCollectionAddress] = useState("");
+  const [NFTs, setNFTs] = useState([]);
+  const [fetchForCollection, setFetchForCollection] = useState(false);
+
+  const fetchNFTsForCollection = async () => {
+    if (collection.length) {
+      var requestOptions = {
+        method: "GET",
+      };
+      const api_key = "FN8guMexPXkWry3c8gKxy8nwCb0LZde-";
+      const baseURL = `https://polygon-mumbai.g.alchemy.com/v2/${api_key}/getNFTsForCollection/`;
+      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
+      const nfts = await fetch(fetchURL, requestOptions).then((data) =>
+        data.json()
+      );
+      if (nfts) {
+        console.log("NFTs in collection:", nfts);
+        setNFTs(nfts.nfts);
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center py-8 gap-y-3">
+      <div className="flex flex-col w-full justify-center items-center gap-y-2">
+        <input
+          className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
+          onChange={(e) => {
+            setCollectionAddress(e.target.value);
+          }}
+          value={collection}
+          type={"text"}
+          placeholder="Add the collection address"
+        ></input>
+        <button
+          className={
+            "disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"
+          }
+          onClick={() => {
+            fetchNFTsForCollection();
+          }}
+        >
+          Search for Polygon Collection Data{" "}
+        </button>
+      </div>
+      <div className="flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center">
+        {NFTs.length &&
+          NFTs.map((nft) => {
+            return <NFTCard nft={nft}></NFTCard>;
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default Main;
